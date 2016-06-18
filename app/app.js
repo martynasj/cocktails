99e2e0f2b4f0aa38e2b017fa18a6eb5634091b90
martynas <mj@svift.net>
2016-06-18 20:49:30 +0200

    Adding tags also generates query string

--- app/app.js
+++ app/app.js
@@ -6,9 +6,10 @@ import ngComponentRouter from 'ngcomponentrouter';
 import 'ng-file-upload';
 import 'angular-carousel';
 import 'angular-touch';
+import 'ng-tags-input';
 
 // Make the main app module and inject dependencies
-const app = angular.module('app', ['ngComponentRouter', 'ngFileUpload', 'angular-carousel']);
+const app = angular.module('app', ['ngComponentRouter', 'ngFileUpload', 'angular-carousel', 'ngTagsInput']);
 
 app.config(function($locationProvider) {
     $locationProvider.html5Mode(true);
--- app/pages/CocktailsPage.js
+++ app/pages/CocktailsPage.js
@@ -1,9 +1,15 @@
 import angular from 'angular';
 import '../components/CocktailCard';
+import { filterByProperty } from '../services/customFilters';
 
 const app = angular.module('app');
 
 function controller(cocktailApi) {
+
+  this.filter = filterByProperty;
+  this.searchInput = '';
+  this.tags = [ ];
+  this.filterQuery = '';
 
   this.dataLoading = false;
   this.error = false;
@@ -19,6 +25,29 @@ function controller(cocktailApi) {
     });
   };
 
+  this.loadTagSuggestions = (query) => {
+    const allPossibleTags = [ 'gin', 'martini', 'vermouth' ];
+    const matchedTags = _.filter(allPossibleTags, (value) => {
+      return value.includes(query);
+    });
+    return matchedTags;
+  }
+
+  this.onTagsChanged = () => {
+    this.filterQuery = '';
+    if (this.tags.length > 0) {
+      this.filterQuery = '?';
+      _.forEach(this.tags, (value, index) => {
+        if (index != 0) {
+          this.filterQuery += '&';
+        }
+        console.log(value, index);
+        const query = `ingredients=${value.text}`;
+        this.filterQuery += query;
+      });
+    }
+  }
+
 }
 
 const template = `
@@ -27,10 +56,31 @@ const template = `
       <div ng-if="$ctrl.dataLoading">Loading...</div>
       <div ng-if="$ctrl.error">No connection to the server</div>
 
+      <!-- Filter -->
       <div class="row">
-        <div class="col-xs-3" ng-repeat="cocktail in $ctrl.allCocktails">
+        <div class="col-xs-12">
+          <div class="form-group">
+            <tags-input ng-model="$ctrl.tags" on-tag-added="$ctrl.onTagsChanged()" on-tag-removed="$ctrl.onTagsChanged()" placeholder="Filter by ingredients...">
+              <auto-complete source="$ctrl.loadTagSuggestions($query)" min-length="1"></auto-complete>
+            </tags-input>
+          </div>
+        </div>
+      </div>
+
+
+
+      <!-- Dev preview -->
+      <pre>filter: {{$ctrl.filterQuery}}</pre>
+
+      <!-- Filtered list of cocktail cards -->
+      <div class="row">
+        <div class="col-xs-3" ng-repeat="cocktail in $ctrl.allCocktails | filter: $ctrl.filter('name', $ctrl.searchInput)">
           <cocktail-card cocktail="cocktail"></cocktail-card>
         </div>
+      </div>
+
+      <div class="row">
+
       </div>
     </div>
 
